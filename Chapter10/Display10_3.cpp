486b18d910177e306feba1941ca33edf4da4d4ff
Neill Henning <neillhenning@gmail.com>
2017-08-31 08:09:26 +0200

    Adding Chapter 10

---
+++ Chapter10/Display10_3.cpp
@@ -0,0 +1,40 @@
+//Program to demonstrate a very simple example of a class.
+//A better version of the class DayOfYear will be given in Display 10.4
+#include <iostream>
+using namespace std;
+class DayOfYear
+{
+public:
+    void output(); //member function declaration
+    int month;
+    int day;
+};
+int main()
+{
+    DayOfYear today, birthday;
+    cout << "Enter today's date:\n";
+    cout << "Enter month as a number: ";
+    cin >> today.month;
+    cout << "Enter the day of the month: ";
+    cin >> today.day;
+    cout << "Enter your birthday:\n";
+    cout << "Enter month as number: ";
+    cin >> birthday.month;
+    cout << "Enter the day of the month: ";
+    cin >> birthday.day;
+    cout << "Today's date is ";
+    today.output();
+    cout << "Your birthday is ";
+    birthday.output();
+    if (today.month == birthday.month && today.day == birthday.day)
+        cout << "Happy Birthday!\n";
+    else
+        cout << "Happy Unbirthday!\n";
+    return 0;
+}
+//Uses iostream
+void DayOfYear::output()
+{
+    cout << "month = " << month
+         << ", day = " << day << endl;
+}
