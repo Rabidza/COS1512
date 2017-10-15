ba18f7965758ab8cd660ef8a81c6efef418f1b8b
Neill Henning <neillhenning@gmail.com>
2017-10-15 19:24:00 +0200

    The Recursive Function power

---
+++ Chapter14/Display14_3.cpp
@@ -0,0 +1,32 @@
+//Program to demonstrate the recursive function power.
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+int power(int x, int n);
+//Precondition: n > = 0;
+//Returns x to the power of n.
+
+int main()
+{
+    for (int n = 0; n < 4; n++)
+        cout << "3 to the power " << n
+             << " is " << power(3, n) << endl;
+
+    return 0;
+}
+
+//Uses iostream and cstdlib:
+int power(int x, int n)
+{
+    if (n < 0)
+    {
+        cout << "Illegal argument to power.\n";
+        exit(1);
+    }
+
+    if (n > 0)
+        return (power(x, n - 1) * x);
+    else // n == 0
+        return (1);
+}
